7b6df007ec44be58bf714b108c7be9178a9ce969
Daniel <dhm7alu@gmail.com>
2014-04-24 18:46:18 +0200

    Revert "Añadida Interfaz de Login"
    This reverts commit 6a5e7e0fec643960acd45163ea3304d1a2261e3b.

--- InfoCosteProgramaGen/InfoCostePrograma/Program.cs
+++ InfoCosteProgramaGen/InfoCostePrograma/Program.cs
@@ -16,7 +16,6 @@ namespace InfoCostePrograma
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Principal());
-            //Application.Run(new Login());
         }
     }
 }
